24f5e1d1c083a17ba8aeefb9e6c7925279fa0c54
Sander Moolin <sandermoolin@gmail.com>
2021-07-28 11:47:07 -0400

    prep

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'MinimumRenderer'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
