fe8f3f051436be772f997ad341ff322f15bad056
Sander Moolin <sandermoolin@gmail.com>
2021-08-04 10:56:50 -0400

    not working yet

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
-import { createRenderer, Component, RendererOptions } from '@vue/runtime-core'
+import { createRenderer, Component, Renderer, RendererOptions, CreateAppFunction } from '@vue/runtime-core'
 import { normalizeContainer } from './utils'
 
-const nodeOps: RendererOptions = {
-    patchProp() {
+// lazy create the renderer - this makes core renderer logic tree-shakable
+// in case the user only imports reactivity utilities from Vue.
+let renderer: Renderer<Element | ShadowRoot>
 
+const nodeOps: RendererOptions<Node, Element> = {
+    patchProp(...args) {
+        console.log('patch prop', args)
     },
     insert(el, parent, anchor) {
+        console.log('insert', { el, parent, anchor })
         parent.insertBefore(el, anchor || null)
-
     },
-    remove() {
-
+    remove(el) {
+        const parent = el.parentNode
+        if (parent) {
+            parent.removeChild(el)
+        }
     },
     createElement(type, isSvg, isCustomizedBuiltIn, vnodeProps) {
+        console.log('create element', { type, isSvg, isCustomizedBuiltIn, vnodeProps })
         const output = document.createElement(type)
         output.classList.add('custom-renderer')
-        // output
         return output
-
     },
     createText(text) {
         return document.createTextNode(text)
@@ -26,30 +32,44 @@ const nodeOps: RendererOptions = {
         return document.createComment(text)
     },
     setText(node, text) {
-        node.innerText = text
+        node.nodeValue = text
     },
     setElementText(node, text) {
-        node.innerText = text
+        node.textContent = text
     },
     parentNode(node) {
-        return node.parentNode
+        return node.parentNode as Element | null
     },
     nextSibling(node) {
         return node.nextSibling
     },
+    querySelector: selector => document.querySelector(selector),
 }
 
-export const createApp = ((root: Component) => {
-    const app = createRenderer(nodeOps).createApp(root)
+function ensureRenderer() {
+    return (
+        renderer ||
+        (renderer = createRenderer<Node, Element | ShadowRoot>(nodeOps))
+    )
+}
+
+export const createApp = ((...args) => {
+    const app = ensureRenderer().createApp(...args)
     const { mount } = app
     app.mount = (containerOrSelector: Element | ShadowRoot | string): any => {
         const container = normalizeContainer(containerOrSelector)
         if (!container) return
 
+        container.innerHTML = ''
         const proxy = mount(container, false, container instanceof SVGElement)
-
+        console.log('proxy', proxy, container)
+        if (container instanceof Element) {
+            container.removeAttribute('v-cloak')
+            container.setAttribute('data-v-app', '')
+        }
         return proxy
     }
+    console.log('app', app)
     return app
 }
-)
+) as CreateAppFunction<Element>
